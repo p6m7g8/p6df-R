0a7fe64845334557705fcf50f1bec42cdf38c8aa
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-05-27 21:05:22 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -1,5 +1,5 @@
 p6df::modules::R::version() { echo "0.0.1" }
-p6df::modules::R::deps()    { 
+p6df::modules::R::deps()    {
 	ModuleDeps=(
 	)
 }
@@ -9,24 +9,26 @@ p6df::modules::R::external::brew() {
 
 p6df::modules::R::init() {
 
-  p6df::modules::R::Renv::init
+#  p6df::modules::R::Renv::init
 }
 
 p6df::modules::R::Renv::init() {
     [ -n "$DISABLE_ENVS" ] && return
 
-    export RENV_ROOT=/Users/pgollucci/.local/share/viking/Renv
-    p6dfz::util::path_if $RENV_ROOT/bin
+    RENV_ROOT=/Users/pgollucci/.local/share/viking/Renv
 
     if [ -x $RENV_ROOT/bin/renv ]; then
+      export RENV_ROOT
       export HAS_RENV=1
+
+      p6dfz::util::path_if $RENV_ROOT/bin
       eval "$(renv init - zsh)"
     fi
 }
 
 p6df::prompt::R::line() {
 
-  env_version "rb"
+  env_version "R"
 }
 
 p6df::modules::R::init
