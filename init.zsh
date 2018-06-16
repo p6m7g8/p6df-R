c2655766f7aada3c18e307a910b35dd0ec52337c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-06-16 17:14:54 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -9,13 +9,15 @@ p6df::modules::R::external::brew() {
 
 p6df::modules::R::init() {
 
-  p6df::modules::R::Renv::init
+  p6df::modules::R::Renv::init "$P6_DFZ_SRC_DIR"
 }
 
 p6df::modules::R::Renv::init() {
+    local dir="$1"
+
     [ -n "$DISABLE_ENVS" ] && return
 
-    RENV_ROOT=/Users/pgollucci/.local/share/viking/Renv
+    RENV_ROOT=$dir/viking/Renv
 
     if [ -x $RENV_ROOT/bin/renv ]; then
       export RENV_ROOT
